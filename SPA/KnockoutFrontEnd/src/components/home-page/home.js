10751775f00b8f98d742b794e42abc6b9f049792
michael.wei@foxmail.com <michael.wei@foxmail.com>
2018-07-10 12:54:48 +0000

    Deploy from Forestry.io

--- SPA/KnockoutFrontEnd/src/components/home-page/home.js
+++
@@ -1,8 +0,0 @@
-define(["knockout", "text!./home.html", "app/search-model"], function (ko, homeTemplate, searchModel) {
-
-  function HomeViewModel(route) {
-      this.search = searchModel;
-  }
-
-  return { viewModel: HomeViewModel, template: homeTemplate };
-});
--- SPA/KnockoutFrontEnd/src/components/investment-filter/investment-filter.js
+++
@@ -1,9 +0,0 @@
-define(['knockout', 'text!./investment-filter.html'], function(ko, templateMarkup) {
-
-  function InvestmentFilter(params) {
-      this.search = params.search;
-  }
-
-  return { viewModel: InvestmentFilter, template: templateMarkup };
-
-});
